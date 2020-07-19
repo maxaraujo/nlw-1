61b53a719b9518874435237713afdf83741475cd
maxaraujo <maximillian.alex@gmail.com>
2020-07-19 12:58:29 -0300

    Iniciando o map na posição onde o computador do usuário está

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -29,6 +29,7 @@ const CreatePoint = () => {
   const [selectedUf, setSelectUf] = useState('0');
   const [selectedCity, setSelectCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -42,6 +43,13 @@ const CreatePoint = () => {
       setUFs(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setInitialPosition([latitude, longitude]);
+    })
+  }, [])
 
   useEffect(() => {
     if(selectedUf === '0')
@@ -123,7 +131,7 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-19.9513896,-44.1994562]} zoom={15} onClick={handleMapClick}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
